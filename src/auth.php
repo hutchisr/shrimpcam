6eb57cc8580456efa042408bf223b93630d8befd
Rachel H <lastexyle@gmail.com>
2018-03-20 22:35:01 -0700

    change stuff

--- src/auth.php
+++ src/auth.php
@@ -4,6 +4,7 @@ header('Content-Type: application/json');
 $accepted_keys = [
   '$2y$10$YzO5LRY6B1OqocMQgh.uleGP2uXHybERTVIMJslm5N42gbnOLkZpO',
   '$2y$10$y9uG5bH/GhDs0OZyeUfhtu2X1IKd6nTGxG682OF2Ccg7AYzrAwnMm',
+  '$2b$12$7Mppc0Ut6AbH.swVhq2dF.dtPRBRQpS1NtKUG1cfUUaEjUSjakoM6'
 ];
 
 // file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'log.json', json_encode($_SERVER, JSON_PRETTY_PRINT));
