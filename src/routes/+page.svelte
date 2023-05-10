e7c5718fa902de4f047a07ade7bc8be8367b6f8b
disarray <pub@disarray.sh>
2023-05-09 21:36:56 -0400

    Add shrimp image and favicon, create time banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Get the current time in PST and update it every second
+  let pstTime = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/Los_Angeles",
+  })
+  setInterval(() => {
+    pstTime = new Date().toLocaleTimeString("en-US", {
+      timeZone: "America/Los_Angeles",
+      hour12: false,
+    })
+  }, 1000)
+
+  // get time in 24 hour format
+</script>
+
+<!-- Shrimpcam hero -->
+<div class="pt-16 pb-8 min-w-full">
+  <div class="flex items-center max-w-fit mx-auto">
+    <h1 class="text-5xl font-medium inline-block mr-4">Shrimpcam!</h1>
+    <img src="/shrimp.png" alt="shrimp" class="w-16 h-16 inline-block" />
+  </div>
+</div>
+<!-- Shrimpcam banner w/ time -->
+<div class="p-4 min-w-full">
+  <div class="flex items-center max-w-fit mx-auto">
+    <!--  -->
+    <div
+      class="p-2.5 rounded-xs outline outline-2 outline-blue-200 bg-blue-400"
+    >
+      <p class="text-white font-bold text-sm">
+        The shrimps' lights are on from 12:00:00 to 21:00:00. The current time
+        is {pstTime}
+      </p>
+    </div>
+  </div>
+</div>
